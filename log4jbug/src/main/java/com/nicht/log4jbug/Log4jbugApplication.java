844f56a0621cba24bd71f7f82b057262fd208ed0
xj1282895175 <1282895175@qq.com>
2022-01-04 09:13:19 +0800

    log4j  Bug 复现

---
+++ log4jbug/src/main/java/com/nicht/log4jbug/Log4jbugApplication.java
@@ -0,0 +1,13 @@
+package com.nicht.log4jbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4jbugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Log4jbugApplication.class, args);
+    }
+
+}
